29497be37ff0ae499680776ed522c7c713b6a3f5
Krishnama  Naidu Senapathi <31931582+coding-yoddha@users.noreply.github.com>
2025-03-02 14:28:56 +0530

    Create highlight.js

---
+++ src/models/highlight.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+const HighlightSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Highlight title is required'],
+    trim: true,
+    maxlength: [100, 'Title cannot be more than 100 characters']
+  },
+  description: {
+    type: String,
+    required: [true, 'Highlight description is required'],
+    trim: true,
+    maxlength: [500, 'Description cannot be more than 500 characters']
+  },
+  image:{
+    data: Buffer,
+    contentType: String,
+  },
+  category: {
+    type: String,
+  },
+  order: {
+    type: Number,
+    default: 0
+  },
+  link: {
+    type: String,
+    trim: true
+  },
+  tags: [{
+    type: String,
+    trim: true
+  }],
+  showInAbout: {
+    type: Boolean
+  },
+  showInMain: {
+    type: Boolean
+  }
+}, {
+  timestamps: true
+});
+
+export default mongoose.models.Highlight || mongoose.model('Highlight', HighlightSchema);
