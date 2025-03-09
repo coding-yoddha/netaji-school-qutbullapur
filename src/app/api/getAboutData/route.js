39dddb98b9dc7bf518e52ec351f5ed7340cf5e5c
Krishnama  Naidu Senapathi <31931582+coding-yoddha@users.noreply.github.com>
2025-03-09 16:18:02 +0530

    Update route.js

--- src/app/api/getAboutData/route.js
+++ src/app/api/getAboutData/route.js
@@ -1,30 +1,30 @@
 import connectToDB from "../../../../config/database";
-import CareerOverview from "@/models/careerOverview";
+import About from "@/models/About";
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
   try {
     await connectToDB();
 
-    const careers = await CareerOverview.find().lean();
+    const careers = await About.find();
 
     // Convert Buffer to Base64 strings for image display
-    const formattedCareers = careers.map((career) => ({
-      _id: career._id.toString(), // Convert _id to a string
-      name: career.name,
-      description: career.description,
-      redirectPageName: career.redirectPageName,
-      image: career.image
-        ? {
-            data: career.image.data.toString("base64"), // Convert Buffer to Base64 if present
-            contentType: career.image.contentType,
-          }
-        : undefined, // If image is not present, set it as undefined
-    }));
+    // const formattedCareers = careers.map((career) => ({
+    //   _id: career._id.toString(), // Convert _id to a string
+    //   name: career.name,
+    //   description: career.description,
+    //   redirectPageName: career.redirectPageName,
+    //   image: career.image
+    //     ? {
+    //         data: career.image.data.toString("base64"), // Convert Buffer to Base64 if present
+    //         contentType: career.image.contentType,
+    //       }
+    //     : undefined, // If image is not present, set it as undefined
+    // }));
 
     return NextResponse.json({
       success: true,
-      data: formattedCareers,
+      data: careers,
     });
   } catch (error) {
     console.error("error: ", error);
