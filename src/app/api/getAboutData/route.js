42847b8980f6a0ffb211a5ef4f59d5cf5a685295
Krishnama  Naidu Senapathi <31931582+coding-yoddha@users.noreply.github.com>
2025-03-09 19:16:11 +0530

    Update route.js

--- src/app/api/getAboutData/route.js
+++ src/app/api/getAboutData/route.js
@@ -1,30 +1,24 @@
 import connectToDB from "../../../../config/database";
 import About from "@/models/About";
+import Highlight from "@/models/Highlight";
+import Faculty from "@/models/faculty"
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
   try {
     await connectToDB();
 
-    const careers = await About.find();
-
-    // Convert Buffer to Base64 strings for image display
-    // const formattedCareers = careers.map((career) => ({
-    //   _id: career._id.toString(), // Convert _id to a string
-    //   name: career.name,
-    //   description: career.description,
-    //   redirectPageName: career.redirectPageName,
-    //   image: career.image
-    //     ? {
-    //         data: career.image.data.toString("base64"), // Convert Buffer to Base64 if present
-    //         contentType: career.image.contentType,
-    //       }
-    //     : undefined, // If image is not present, set it as undefined
-    // }));
-
+    const data = await About.find();
+    const hightlights = await Highlight.find({showInAbout: true});
+    const faculty = await Faculty.find({showInAbout: true});
+    const response = {
+      about: data[0],
+      hightlights,
+      faculty
+    };
     return NextResponse.json({
       success: true,
-      data: careers,
+      response,
     });
   } catch (error) {
     console.error("error: ", error);
