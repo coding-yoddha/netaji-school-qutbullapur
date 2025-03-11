e19626463b39fa8d952f4980c37ef854dabb7b17
Krishnama  Naidu Senapathi <31931582+coding-yoddha@users.noreply.github.com>
2025-03-11 21:20:51 +0530

    Create apiHelpers.ts

---
+++ src/app/utils/apiHelpers.ts
@@ -0,0 +1,54 @@
+const fetchAboutPageData = async () => {
+    const res = await fetch(`/api/getAboutData`, {
+      method: "GET",
+    });
+    if (!res.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await res.json();
+    return data;
+  };
+  
+  const fetchContactPageData = async () => {
+    const res = await fetch(`/api/getContact`, {
+      method: "GET",
+    });
+    if (!res.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await res.json();
+    return data;
+  };
+  
+  const fetchFacultyPageData = async () => {
+    const res = await fetch(
+      `/api/getFaculty`,
+      {
+        method: "GET",
+      }
+    );
+    if (!res.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await res.json();
+    return data;
+  };
+  
+  const fetchMainPageData = async () => {
+    const res = await fetch(`/api/getMainPage`, {
+      method: "GET",
+    });
+    if (!res.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await res.json();
+    return data;
+  };
+  
+  export {
+    fetchAboutPageData,
+    fetchContactPageData,
+    fetchFacultyPageData,
+    fetchMainPageData,
+  };
+  
