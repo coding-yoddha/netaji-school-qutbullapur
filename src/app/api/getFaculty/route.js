f25a956e3bcdeb5de5a524992a266a25e89688cf
Krishnama  Naidu Senapathi <31931582+coding-yoddha@users.noreply.github.com>
2025-05-31 12:43:18 +0530

    Update route.js

--- src/app/api/getFaculty/route.js
+++ src/app/api/getFaculty/route.js
@@ -5,11 +5,19 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     await connectToDB();
-    const faculty = await Faculty.find({ showInFacultyPage: true });
-    
+    var faculties = await Faculty.find({ showInFacultyPage: true });
+    faculties = faculties.map((faculty) => ({
+      ...faculty._doc,
+      profilePicture: faculty.profilePicture
+        ? {
+            data: faculty.profilePicture.data.toString("base64"), // Convert Buffer to Base64 if present
+            contentType: faculty.profilePicture.contentType,
+          }
+        : undefined, // If image is not present, set it as undefined
+    }));
     return NextResponse.json({
       success: true,
-      response: faculty,
+      response: faculties,
     });
   } catch (error) {
     console.error("error: ", error);
