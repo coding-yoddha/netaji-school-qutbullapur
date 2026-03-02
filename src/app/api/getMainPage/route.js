9b78ba5338d0e65ed23de47bbb052027ae7b0eb8
Krishnama  Naidu Senapathi <31931582+coding-yoddha@users.noreply.github.com>
2026-03-02 12:18:35 +0530

    Optimize data fetching in GET function
    Refactor GET function to use Promise.all for concurrent data fetching and remove unused imports and variables.

--- src/app/api/getMainPage/route.js
+++ src/app/api/getMainPage/route.js
@@ -1,10 +1,8 @@
 import connectToDB from "../../../../config/database";
 import School from "@/models/school";
 import Highlight from "@/models/highlight";
-import MainPageImage from "@/models/mainPageImage";
 import Achievement from "@/models/achievement";
 import Event from "@/models/event";
-import Review from "@/models/review";
 import Contact from "@/models/contact";
 import { NextResponse } from "next/server";
 
@@ -12,18 +10,23 @@ export async function GET() {
   try {
     await connectToDB();
 
-    const data = await School.find();
-    const schoolData = {
-      ...data[0]._doc,
-      logo: data[0]
-        ? {
-            data: data[0].logo.data.toString("base64"), // Convert Buffer to Base64 if present
-            contentType: data[0].logo.contentType,
-          }
-        : undefined,
-    };
-    let hightlights = await Highlight.find({ showInMain: true });
-    hightlights = hightlights.map((hightlight) => ({
+    const [
+      schoolDocs,
+      highlightDocs,
+      achievement,
+      contact,
+      events,
+    ] = await Promise.all([
+      School.find().select('name description').lean(),
+      Highlight.find({ showInMain: true }),
+      Achievement.find().lean(),
+      Contact.find().select('socialMedia').lean(),
+      Event.find().select('-image').sort({ order: 1 }).lean(),
+    ]);
+
+    const schoolData = schoolDocs[0] || {};
+
+    const hightlights = highlightDocs.map((hightlight) => ({
       ...hightlight._doc,
       image: hightlight.image
         ? {
@@ -32,28 +35,13 @@ export async function GET() {
           }
         : undefined, // If image is not present, set it as undefined
     }));
-    let mainPageImage = [];
-    mainPageImage = mainPageImage.map((event) => ({
-      ...event._doc,
-      image: event.image
-        ? {
-            data: event.image.data.toString("base64"), // Convert Buffer to Base64 if present
-            contentType: event.image.contentType,
-          }
-        : undefined, // If image is not present, set it as undefined
-    }));
-    const achievement = await Achievement.find();
-    const review = await Review.find();
-    const contact = await Contact.find();
-    const events = await Event.find().sort({ order: 1 });
 
     const response = {
       schoolData: schoolData,
       hightlights,
-      eventImages: mainPageImage,
+      eventImages: [],
       achievement,
-      review,
-      socialMedia: contact[0].socialMedia,
+      socialMedia: contact[0]?.socialMedia,
       events,
     };
     return NextResponse.json({
